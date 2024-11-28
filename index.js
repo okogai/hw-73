28198dc5cee658837db06713977a078e55c4a41f
Oleg <evolrn89@gmail.com>
2024-11-28 11:21:05 +0600

    Task1 complete

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/', (req, res) => {
+    const message = 'Main page';
+    res.send(message);
+});
+
+app.get('/:message', (req, res) => {
+    const message = req.params.message;
+    res.send(message);
+});
+
 app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`);
 });
